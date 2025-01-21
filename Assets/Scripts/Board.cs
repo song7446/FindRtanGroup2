2a308165e1807cb69fd03fae6afc78f79b35e873
psj1208 <162458734+psj1208@users.noreply.github.com>
2025-01-21 12:32:03 +0900

    카드 프리팹 변경

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -9,10 +9,10 @@ public class Board : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
-        arr = arr.OrderBy(x => Random.Range(0f, 7f)).ToArray();
+        int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9 };
+        arr = arr.OrderBy(x => Random.Range(0f, 9f)).ToArray();
 
-        for (int i = 0; i < 16; i++)
+        for (int i = 0; i < 20; i++)
         {
             GameObject go = Instantiate(card, this.transform);
 
