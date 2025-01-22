604f923e28fc43f24664449891d8bddc7111971b
2025Unity <ips1966@naver.com>
2025-01-22 11:54:31 +0900

    설정 화면 수정

--- Assets/Scripts/SettingButton.cs
+++ Assets/Scripts/SettingButton.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class SettingButton : MonoBehaviour
 {
     public GameObject SettingCanvas;
+    public GameObject StartButton;
+    public GameObject DifficultyButton;
+    public GameObject ExitButton;
+
+
     // Start is called before the first frame update
     public void OpenSetting()
     {
         SettingCanvas.SetActive(true);
+        StartButton.SetActive(false);
+        DifficultyButton.SetActive(false);
+        ExitButton.SetActive(false);
     }
 
     public void CloseSetting()
     {
         SettingCanvas.SetActive(false);
+        StartButton.SetActive(true);
+        DifficultyButton.SetActive(true);
+        ExitButton.SetActive(true);
     }
 }
