78f5b4ec746e138df275e555483f434f394caa4e
psj1208 <162458734+psj1208@users.noreply.github.com>
2025-01-23 16:08:33 +0900

    주석

--- Assets/Scripts/DifficultyManager.cs
+++ Assets/Scripts/DifficultyManager.cs
@@ -22,18 +22,22 @@ public class DifficultyManager : MonoBehaviour
         }
     }
 
+    //스테이지 당 제한 시간을 담아둔 배열(인스펙터에서 조절 가능)
     public float[] timeArr = { 60.0f, 45.0f, 30.0f, 60.0f };
     public float settedTime = 60.0f;
     public int settedStage = 0;
 
+    //스테이지의 해금 여부(1,2,3,히든)
     public bool[] stageClear = { true, false, false, false };
-
+    
+    //스테이지 당 제한 시간 설정
     public void timeSet(int stage)
     {
         settedStage = stage;
         settedTime = timeArr[stage];
     }
-
+    
+    //스테이지 해금 기능
     public void stageOpen()
     {
         settedStage += 1;
